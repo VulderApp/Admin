1241405f1c9667ab611851aba80ae32ecfad165c
MRmlik12 <mrmlik12sup@gmail.com>
2021-08-31 23:24:37 +0200

    Add SchoolForm database model

---
+++ src/Vulder.Admin.Core/ProjectAggregate/SchoolForm/SchoolForm.cs
@@ -0,0 +1,36 @@
+using System;
+using Vulder.SharedKernel;
+using Vulder.SharedKernel.Interface;
+
+namespace Vulder.Admin.Core.ProjectAggregate.SchoolForm
+{
+    public class SchoolForm : BaseEntity, IAggregateRoot
+    {
+        public Guid RequesterId { get; set; }
+        public string SchoolName { get; set; }
+        public string TimetableUrl { get; set; }
+        public string SchoolUrl { get; set; }
+        public DateTimeOffset CreateAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public Guid ApprovedBy { get; set; }
+        public DateTimeOffset ApprovedAt { get; set; }
+
+        public SchoolForm CreateTimestamp()
+        {
+            CreateAt = DateTimeOffset.Now;
+            return this;
+        }
+
+        public SchoolForm UpdateTimestamp()
+        {
+            UpdatedAt = DateTimeOffset.Now;
+            return this;
+        }
+
+        public SchoolForm ApprovedTimestamp()
+        {
+            ApprovedAt = DateTimeOffset.Now;
+            return this;
+        }
+    }
+}
--- src/Vulder.Admin.Core/ProjectAggregate/User/NewUser.cs
+++
@@ -1,9 +0,0 @@
-namespace Vulder.Admin.Core.ProjectAggregate.User
-{
-    public class NewUser
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string SchoolUrl { get; set; }
-    }
-}
