8704362e64025f2194ef1ea3ad7a4cef80717c50
MRmlik12 <mrmlik12sup@gmail.com>
2022-01-17 12:19:59 +0100

    Add integration test

---
+++ test/Vulder.Admin.UnitTests/Core/Validators/ChangePasswordModelValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation.TestHelper;
+using Vulder.Admin.Core.Models;
+using Vulder.Admin.Core.Validators;
+using Xunit;
+
+namespace Vulder.Admin.UnitTests.Core.Validators;
+
+public class ChangePasswordModelValidatorTest
+{
+
+    [Fact]
+    public void ValidateChangePasswordModel_Correct()
+    {
+        var model = new ChangePasswordModel
+        {
+            CurrentPassword = "XSfsT1bvbPHLwCcaPYq/Waum5I8EUSZ1aTLy1bo/Gz0=",
+            NewPassword = "NzjDED/7tqsSFI62KRwHyBe8eHOFjGbyiDw/M1BEOEw="
+        };
+
+        var result = new ChangePasswordModelValidator().TestValidate(model).IsValid;
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateChangePasswordModel_NotValid()
+    {
+        var model = new ChangePasswordModel
+        {
+            CurrentPassword = "example",
+            NewPassword = "example"
+        };
+
+        var result = new ChangePasswordModelValidator().TestValidate(model).IsValid;
+
+        Assert.False(result);
+    }
+}
