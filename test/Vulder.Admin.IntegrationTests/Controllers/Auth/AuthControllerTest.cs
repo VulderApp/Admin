8047cadb7a2abc420aa11ca5719706e27570b37f
MRmlik12 <mrmlik12sup@gmail.com>
2022-03-23 09:05:15 +0100

    Add AuthControllerTest.cs

--- test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
+++ test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -7,8 +7,7 @@ using Xunit;
 
 namespace Vulder.Admin.IntegrationTests.Controllers.Auth;
 
-[Collection("Register Collection")]
-public class RegisterControllerTest
+public class AuthControllerTest
 {
     [Fact]
     public async void POST_1_RegisterController_200_StatusCode()
@@ -44,5 +43,31 @@ public class RegisterControllerTest
         using var response = await client.PostAsync("/auth/Register", httpContent);
 
         Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+    }
+    
+    [Fact]
+    public async void POST_3_LoginController_200_StatusCode()
+    {
+        var registerModel = new RegisterUserModel
+        {
+            Email = "login@example.com",
+            Password = "nevergiveup"
+        };
+
+        var loginModel = new LoginUserModel
+        {
+            Email = registerModel.Email,
+            Password = registerModel.Password
+        };
+
+        await using var application = new WebServerFactory();
+        using var client = application.CreateClient();
+        var httpContent = new StringContent(JsonSerializer.Serialize(registerModel), Encoding.UTF8, "application/json");
+        await client.PostAsync("/auth/Register", httpContent);
+
+        httpContent = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
+        using var response = await client.PostAsync("/auth/Login", httpContent);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
