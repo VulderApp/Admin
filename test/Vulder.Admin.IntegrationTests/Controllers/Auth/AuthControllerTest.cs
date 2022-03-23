d22964ab43680c57381fe4d5d8ff651aa51b0233
MRmlik12 <mrmlik12sup@gmail.com>
2022-03-23 09:13:29 +0100

    Fix login credentials

--- test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
+++ test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
@@ -48,24 +48,17 @@ public class AuthControllerTest
     [Fact]
     public async void POST_3_LoginController_200_StatusCode()
     {
-        var registerModel = new RegisterUserModel
-        {
-            Email = "login@example.com",
-            Password = "nevergiveup"
-        };
-
         var loginModel = new LoginUserModel
         {
-            Email = registerModel.Email,
-            Password = registerModel.Password
+            Email = "example@example.com",
+            Password =
+                "dg==OXcEoiOUA2P1IxTWUjcX5XeSQsOz1kJE9+K01leUUmR2TEFaKX5Ge1txUj1yQVNdZTdsKUt7IWBNVXlsYUYtRmozQjg/eSEg"
         };
 
         await using var application = new WebServerFactory();
         using var client = application.CreateClient();
-        var httpContent = new StringContent(JsonSerializer.Serialize(registerModel), Encoding.UTF8, "application/json");
-        await client.PostAsync("/auth/Register", httpContent);
-
-        httpContent = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
+        
+        var httpContent = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync("/auth/Login", httpContent);
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
