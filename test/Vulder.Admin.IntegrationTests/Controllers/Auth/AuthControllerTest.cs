129eff0fd27aa7b3fe8b0a11ce8fc560a89e86b6
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-12 19:00:17 +0200

    Cleanup Tests

--- test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
+++ test/Vulder.Admin.IntegrationTests/Controllers/Auth/AuthControllerTest.cs
@@ -1,66 +1,82 @@
 ﻿using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Text.Json;
+using Newtonsoft.Json;
 using Vulder.Admin.Core.Models;
+using Vulder.Admin.Core.ProjectAggregate.User.Dtos;
+using Vulder.Admin.IntegrationTests.Fixtures;
 using Xunit;
 
 namespace Vulder.Admin.IntegrationTests.Controllers.Auth;
 
+[Collection("Controllers collection")]
 public class AuthControllerTest
 {
+    private readonly AdminCredentialsFixture _adminCredentials;
+
+    public AuthControllerTest(AdminCredentialsFixture adminCredentials)
+    {
+        _adminCredentials = adminCredentials;
+    }
+
     [Fact]
     public async void POST_1_RegisterController_200_StatusCode()
     {
         var body = new RegisterUserModel
         {
-            Email = "example@example.com",
+            Email = _adminCredentials.Email,
             Password =
-                "dg==OXcEoiOUA2P1IxTWUjcX5XeSQsOz1kJE9+K01leUUmR2TEFaKX5Ge1txUj1yQVNdZTdsKUt7IWBNVXlsYUYtRmozQjg/eSEg"
+                _adminCredentials.Password
         };
 
         await using var application = new WebServerFactory();
         using var client = application.CreateClient();
-        var httpContent = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+        var httpContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync("/auth/Register", httpContent);
+        var registerUserModel = JsonConvert.DeserializeObject<AuthUserDto>(await response.Content.ReadAsStringAsync());
+
+        _adminCredentials.Token = registerUserModel?.Token;
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(registerUserModel?.Token);
     }
-    
+
     [Fact]
     public async void POST_2_RegisterController_500_StatusCode()
     {
         var body = new RegisterUserModel
         {
-            Email = "example@example.com",
+            Email = _adminCredentials.Email,
             Password =
-                "dg==XE=cEoiOUA2P1IxTWUjcX5XeSQsOz1kJE9+K01leUg=UmR2TEFaKX5Ge1txUj1yQVNdZTdsKUt7IWBNVXlsYUYtRmozQjg/eSEg"
+                "dg==XE=cEoiOUA2P1IxTWUjcX5XeSQsOz1kJE9+K01leUg=UmR2aKX5Ge1txUj1yQVNdZTdsKUt7IWBNVXlsYUYtRmozQjg/eSEg"
         };
 
         await using var application = new WebServerFactory();
         using var client = application.CreateClient();
-        var httpContent = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+        var httpContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync("/auth/Register", httpContent);
 
         Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
     }
-    
+
     [Fact]
     public async void POST_3_LoginController_200_StatusCode()
     {
-        var loginModel = new LoginUserModel
+        var body = new LoginUserModel
         {
-            Email = "example@example.com",
+            Email = _adminCredentials.Email,
             Password =
-                "dg==OXcEoiOUA2P1IxTWUjcX5XeSQsOz1kJE9+K01leUUmR2TEFaKX5Ge1txUj1yQVNdZTdsKUt7IWBNVXlsYUYtRmozQjg/eSEg"
+                _adminCredentials.Password
         };
 
         await using var application = new WebServerFactory();
         using var client = application.CreateClient();
-        
-        var httpContent = new StringContent(JsonSerializer.Serialize(loginModel), Encoding.UTF8, "application/json");
+
+        var httpContent = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
         using var response = await client.PostAsync("/auth/Login", httpContent);
+        var registerUserModel = JsonConvert.DeserializeObject<AuthUserDto>(await response.Content.ReadAsStringAsync());
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(registerUserModel?.Token);
     }
 }
